7ec93cc2ba88811289fa99ae60911307d6e8a441
samowolabi1 <soludareowolabi@gmail.com>
2022-11-01 12:55:00 +0100

    link sections

--- src/App.js
+++ src/App.js
@@ -16,16 +16,6 @@ function App() {
         >
           Twitter
         </a>
-
-        <a
-          className="button"
-          href="https://training.zuri.team/"
-          target="_blank"
-          id='btn_zuri'
-          rel="noopener noreferrer"
-        >
-          Zuri Team
-        </a>
       
         <a
           className="button"
@@ -36,6 +26,49 @@ function App() {
         >
           Zuri Team
         </a>
+
+        <a
+          className="button"
+          href="http://books.zuri.team"
+          target="_blank"
+          id='books'
+          rel="noopener noreferrer"
+        >
+          Books
+        </a>
+
+        <a
+          className="button"
+          href="https://books.zuri.team/"
+          target="_blank"
+          id='book_python'
+          rel="noopener noreferrer"
+        >
+          Python Books
+        </a>
+
+        
+        <a
+          className="button"
+          href="https://background.zuri.team"
+          target="_blank"
+          id='book_python'
+          rel="noopener noreferrer"
+        >
+          Background Check for Coders
+        </a>
+
+        <a
+          className="button"
+          href="https://books.zuri.team/design-rules"
+          target="_blank"
+          id='book_design'
+          rel="noopener noreferrer"
+        >
+          Design Books
+        </a>
+
+
       </header>
     </div>
   );
